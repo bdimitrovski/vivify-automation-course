8f153270a906625611913aa1712310111ffe7955
Bojan Dimitrovski <bd1637@tobii.com>
2022-05-27 22:12:42 +0200

    Remove POM code

--- cypress/support/classes/boards/index.js
+++ cypress/support/classes/boards/index.js
@@ -2,45 +2,6 @@ import Utils from '../utils';
 import BoardElements from '../../elements/board-elements';
 
 const utils = new Utils();
-class Boards {
-    setupTests(options) {
-        if (options && options.resetDB) {
-            cy.request('DELETE', '/api/boards');
-        }
-
-        utils.visitUrl('/');
-    }
-
-    createBoard() {
-        cy.get(BoardElements.createBoard).click();
-    }
-
-    addBoard(boardName) {
-        cy.get(BoardElements.createBoard).click();
-        cy.get(BoardElements.newBoardInput).type(`${boardName}{enter}`);
-    }
-
-    assertBoardName(boardName) {
-        cy.get(BoardElements.boardTitle).then(($title) => {
-            expect($title[0]._value).to.equal(boardName);
-        })
-    }
-
-    createNewBoardFromApi(url) {
-        return cy.task('fetchData', {
-            url: url
-        });
-    }
-
-    returnToBoardsView() {
-        cy.get(BoardElements.boardsView).click();
-    }
-
-    deleteBoard(boardName) {
-        cy.contains(boardName).click();
-        cy.get(BoardElements.boardOptions).click();
-        cy.get(BoardElements.deleteBoard).click();
-    }
-}
+class Boards {}
 
 export default Boards;
