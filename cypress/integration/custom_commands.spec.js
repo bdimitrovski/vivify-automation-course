d8bf521997f94538c2c63d15c2d6fb95cf578709
MajaC021 <majacveticanin90@gmail.com>
2022-06-11 22:08:53 +0200

    first commit

--- cypress/integration/custom_commands.spec.js
+++ cypress/integration/custom_commands.spec.js
@@ -5,23 +5,53 @@ describe('Creating and using custom commands', () => {
     })
 
     it('Create a new board via the UI', () => {
+        cy.addBoard('New board')
+        cy.returnToBoardsView();
 
+        cy.contains('New board').should("be.visible")
     });
 
     it('Login via UI', () => {
+        cy.loginViaUI();
+    });
 
+    it('Login via API', () => {
+        cy.loginViaApi();
     });
 
     it('Can create a new board via API and edit it', () => {
+        cy.log('create a new board via API...').addBoardApi('surfboard')
+        cy.visit('/')
+        cy.get('[data-cy=board-item]').should('contain.text', 'surfboard')
 
+        cy.updateBoardApi('updated board name');
+
+        cy.get('[data-cy=board-item]').should('contain.text', 'updated board name')
     });
 
     it('Can create a list via API', () => {
-
+        cy.addBoardApi('nice board')
+        cy.visit('/')
+        cy.contains('nice board').click()
+        cy.addListApi({
+            boardId: 0,
+            title: 'new list'
+        })
     });
 
     it('Open task detail', () => {
-
+        cy.addBoardApi("bbb")
+        cy.contains('bbb').click()
+        cy.addListApi({
+            boardId: 0,
+            title: 'new list',
+        })
+        .addTaskApi({
+            boardId: 0,
+            listId: 0,
+            title: 'new taskkk'
+        })
+        cy.addTaskDesc('helloooo')
     });
 
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,42 +1,108 @@
 import BoardElements from '../support/elements/board-elements';
 import LoginElements from '../support/elements/login-elements';
 
-Cypress.Commands.add('resetDB', () => {
-
+Cypress.Commands.add('resetDB', (type) => {
+    switch (type) {
+        case 'boards':
+            cy.request('DELETE', '/api/boards')
+            break;
+        case 'lists':
+            cy.request('DELETE', '/api/lists')
+            break;
+        case 'tasks':
+            cy.request('DELETE', '/api/tasks')
+            break;
+        case 'users':
+            cy.request('DELETE', '/api/users')
+            break;
+        case 'all':
+            cy.request('DELETE', '/api/reset')
+            break;
+        default:
+            throw new Error('Type of DB reset must be specificed!')
+    }
 });
 
-Cypress.Commands.add('loginViaUI', () => {
-
+Cypress.Commands.add('loginViaUI', (username = Cypress.env('username'), password = Cypress.env('password')) => {
+    cy.get(LoginElements.loginMenu).click();
+    cy.get(LoginElements.email).type(username);
+    cy.get(LoginElements.password).type(password);
+    cy.get(LoginElements.loginButton).click();
 });
 
 Cypress.Commands.add('logout', () => {
-
+    cy.get(['[data-cy="logged-user"]']).click();
+    cy.get(['[data-cy="logout"]']).click();
 });
 
-Cypress.Commands.add('addBoard', () => {
-
+Cypress.Commands.add('addBoard', (boardname) => {
+    cy.get(BoardElements.createBoard).click()
+    cy.get(BoardElements.newBoardInput).type(`${boardname}{enter}`)
 });
 
 Cypress.Commands.add('returnToBoardsView', () => {
-
+    cy.get(BoardElements.boardsView).click();
 });
 
-Cypress.Commands.add('addBoardApi', () => {
+Cypress.Commands.add('loginViaApi', (username = Cypress.env('username'), password = Cypress.env('password')) =>{
+    cy.request({
+        method: "POST",
+        url: "/login",
+        body: {
+            email: username,
+            password: password
+        }
+    }).as('login').then((response) => {
+        expect(response.status).to.eq(200)
+        expect(response.body.accessToken).to.have.length(179)
+    })
 
+})
+
+Cypress.Commands.add('addBoardApi', (name) => {
+    cy.request('POST', '/api/boards', {name})
+    .then(({body}) => {
+        Cypress.env('boards').push(body)
+    })
 });
 
-Cypress.Commands.add('addListApi', () => {
-
+Cypress.Commands.add('updateBoardApi', (name, index = 0) => {
+    cy.request(
+        'PATCH',
+        `/api/boards/${Cypress.env('boards')[index].id}`, {name}
+    ).then(({body}) => {
+        Cypress.env('boards')[index] = body;
+    })
 });
 
-Cypress.Commands.add('addTaskApi', () => {
-
+Cypress.Commands.add('addListApi', (title, boardId = 0) => {
+    cy.request('POST', '/api/lists', {boardId: Cypress.env('boards')[boardId].id, title})
+    .then(({body}) => {
+        Cypress.env('lists').push(body) 
+    })
 });
 
-Cypress.Commands.add('updateBoardApi', () => {
-
+Cypress.Commands.add('addTaskApi', ({title, boardId = 0, listId = 0}) => {
+    cy.request(
+        'POST',
+        '/api/tasks',
+        {title,
+        boardId: Cypress.env('boards')[boardId].id,
+        listId: Cypress.env('lists')[listId].id
+    }).then(({body}) => {
+        Cypress.env('tasks').push(body) 
+    })
+});
+Cypress.Commands.add('addTaskDesc', (desc) => {
+    cy.get('[data-cy=task-module]').click()
+    cy.get('[data-cy=task-description]').click()
+    cy.get('[data-cy=task-description-input]').type(desc)
+    cy.get('[data-cy=task-description-save]').click()
 });
 
+Cypress.Commands.add('deleteBoardApi', () => {
+
+});
 Cypress.Commands.add('deleteBoardApi', () => {
 
 });
